6f72c251da4335eec088e617e5648360485ae0b5
Marinosantellan <48847515+Marinosantellan@users.noreply.github.com>
2020-08-08 21:31:26 -0300

    Add files via upload

---
+++ ADT_Vector.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INIT_SIZE 1000
+
+typedef struct
+{
+
+	void **elements;
+	size_t len;
+	size_t alloc_size;
+
+	destructor_t destructor;
+	comparator_t comparator;
+	printer_t 	 printer;
+
+} ADT_Vector_t;
+
+/*Función que crea un ADT_Vector_t y se lo asigna al puntero recibido
+  el vector devuelto ya tiene memoria pedida e inicializada por default*/
+status_t ADT_Vector_create( ADT_Vector_t **vector)/*En realidad devuelve un ADT_Error_t*/
+{
+	size_t aux_size = INIT_SIZE;
+
+	if( vector == NULL)
+	{
+		return ERROR_NULL_POINTER;
+	}
+
+	if((*vector = (ADT_Vector_t*) malloc(sizeof(ADT_Vector_t))) == NULL )
+	{
+		return ERROR_NO_MEMORY;
+	}
+
+	(*vector)->len = 0;
+	(*vector)->alloc_size = 0;
+
+	(*vector)->destructor = NULL;
+	(*vector)->comparator = NULL;
+	(*vector)->printer    = NULL; 
+
+	if(((*vector)->elements = (void **) calloc( aux_size , sizeof(void *))) == NULL)
+	{
+		ADT_Vector_destroy(vector);
+		return ERROR_NO_MEMORY;
+	}
+
+	(*vector)->alloc_size = aux_size;
+
+	return OK;
+}
+
+status_t ADT_Vector_destroy(ADT_Vector_t **vector)
+{
+	size_t i;
+
+	if(vector == NULL)
+	{
+		return ERROR_NULL_POINTER;
+	}
+
+	for( i = 0 ; i < (*vector)->len ; i++)
+	{
+		destructor(((*vector)->elements)[i]);
+
+	}
+
+	free(elements);
+
+	free(*vector);
+
+	*vector = NULL;
+
+	return OK;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
